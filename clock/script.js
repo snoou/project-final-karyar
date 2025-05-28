c845cf39435df4a48e2947ac713cfd2856f1a905
SAJJAD <122774486+snoou@users.noreply.github.com>
2025-05-28 12:59:16 +0330

    Update script.js

--- clock/script.js
+++ clock/script.js
@@ -15,7 +15,7 @@
           hr.style.transform = `rotateZ(${(hh*30)+(mm*6/12)}deg)`;
           mn.style.transform = `rotateZ(${mm*6}deg)`;
           sc.style.transform = `rotateZ(${ss*6}deg)`; 
-          if (hh<=12){
+          if (hh<12){
               digit.innerHTML = `<p> ${String(hh).padStart(2, '0')} : ${String(mm).padStart(2 , '0')} : ${String(ss).padStart(2, '0')} Am</p>`
           } else{
               digit.innerHTML = `<p> ${String(hh-12).padStart(2, '0')} : ${String(mm).padStart(2 , '0')} : ${String(ss).padStart(2, '0')} Pm</p>`
